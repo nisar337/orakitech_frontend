8117f4e91c0561e420dec7eb70abc87322a605c7
nisar337 <hopefull77778@gmail.com>
2026-05-28 23:51:59 +0500

    add the other sub-catagory on admin and user side

--- src/components/nav-components/CategoryData.js
+++ src/components/nav-components/CategoryData.js
@@ -1,7 +1,7 @@
 const CategoryData = [
   {
-    "New Laptop": ["HP", "Dell", "Lenovo", "Acer", "Macbook"],
-    "Used Laptop": ["HP", "Dell", "Lenovo", "Acer", "Macbook", "G Tech"],
+    "New Laptop": ["HP", "Dell", "Lenovo", "Acer", "Macbook", "Other"],
+    "Used Laptop": ["HP", "Dell", "Lenovo", "Acer", "Macbook", "G Tech", "Other"],
     Accessories: ["Charger", "Keyboard", "Mouse", "Mouse Pad"],
     "External Hardrive": ["SSD", "HDD", "USB", "RAM", "External Hard Drive"],
   },
